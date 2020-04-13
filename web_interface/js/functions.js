d6bbb8ed174c9816ed2aff247a396193ced9ebaa
RomeshEtulgama <etulgamaromesh79@gmail.com>
2020-04-13 12:57:36 +0530

    Improved invoices table. Table data needs a rework. (Inside add_row() function js)

--- web_interface/js/functions.js
+++ web_interface/js/functions.js
@@ -1,6 +1,6 @@
 // Client
 
-function submitAddClientForm(){
+function submitAddClientForm() {
   var index = $('#InputINDEX').val();
   var name = $('#InputNAME').val();
   var address = $('#InputADDRESS').val();
@@ -11,32 +11,32 @@ function submitAddClientForm(){
   var defaultproduct = $('#InputPRODUCT').val();
   var remarks = $('#InputREMARKS').val();
 
-  if(name.trim() == '' ){
-        alert('Please enter client name.');
-        $('#InputNAME').focus();
-        return false;
-    }else if(address.trim() == '' ){
-        alert('Please enter client address.');
-        $('#InputADDRESS').focus();
-        return false;
-    } else {
-      
-      $.post("./functions.php", {
-        sub:"add_client_form",
-        sub_index: index,
-        sub_name: name,
-        sub_address: address,
-        sub_nickname: nickname,
-        sub_code: code,
-        sub_contact1: contact1,
-        sub_contact2: contact2,
-        sub_defaultproduct: defaultproduct,
-        sub_remarks: remarks
-      });
-    }
+  if (name.trim() == '') {
+    alert('Please enter client name.');
+    $('#InputNAME').focus();
+    return false;
+  } else if (address.trim() == '') {
+    alert('Please enter client address.');
+    $('#InputADDRESS').focus();
+    return false;
+  } else {
+
+    $.post("./functions.php", {
+      sub: "add_client_form",
+      sub_index: index,
+      sub_name: name,
+      sub_address: address,
+      sub_nickname: nickname,
+      sub_code: code,
+      sub_contact1: contact1,
+      sub_contact2: contact2,
+      sub_defaultproduct: defaultproduct,
+      sub_remarks: remarks
+    });
+  }
 }
 
-function submitEditClientForm(){
+function submitEditClientForm() {
   var id = $('#edit_InputID').val();
   var index = $('#edit_InputINDEX').val();
   var name = $('#edit_InputNAME').val();
@@ -48,42 +48,42 @@ function submitEditClientForm(){
   var defaultproduct = $('#edit_InputPRODUCT').val();
   var remarks = $('#edit_InputREMARKS').val();
 
-  if(name.trim() == '' ){
-        alert('Please enter client name.');
-        $('#edit_InputNAME').focus();
-        return false;
-    }else if(address.trim() == '' ){
-        alert('Please enter client address.');
-        $('#edit_InputADDRESS').focus();
-        return false;
-    } else {
-      
-      $.post("./functions.php", {
-        sub:"edit_client_form",
-        sub_id: id,
-        sub_index: index,
-        sub_name: name,
-        sub_address: address,
-        sub_nickname: nickname,
-        sub_code: code,
-        sub_contact1: contact1,
-        sub_contact2: contact2,
-        sub_defaultproduct: defaultproduct,
-        sub_remarks: remarks
-      });      
-    }
+  if (name.trim() == '') {
+    alert('Please enter client name.');
+    $('#edit_InputNAME').focus();
+    return false;
+  } else if (address.trim() == '') {
+    alert('Please enter client address.');
+    $('#edit_InputADDRESS').focus();
+    return false;
+  } else {
+
+    $.post("./functions.php", {
+      sub: "edit_client_form",
+      sub_id: id,
+      sub_index: index,
+      sub_name: name,
+      sub_address: address,
+      sub_nickname: nickname,
+      sub_code: code,
+      sub_contact1: contact1,
+      sub_contact2: contact2,
+      sub_defaultproduct: defaultproduct,
+      sub_remarks: remarks
+    });
+  }
 }
 
-function deleteClient(id){
+function deleteClient(id) {
   $.post("./functions.php", {
-    sub:"delete_client",
+    sub: "delete_client",
     sub_id: id
-  });   
+  });
 }
 
 // Products
 
-function submitAddProductForm(){
+function submitAddProductForm() {
   var index = $('#productInputINDEX').val();
   var name = $('#productInputNAME').val();
   var description = $('#productInputDESCRIPTION').val();
@@ -91,63 +91,63 @@ function submitAddProductForm(){
   var remarks = $('#productInputREMARKS').val();
   var clients = $('#productSelectCLIENTS').val();
 
-  if(name.trim() == '' ){
-        alert('Please enter product name.');
-        $('#productInputINDEX').focus();
-        return false;
-    }else if(description.trim() == '' ){
-        alert('Please enter description address.');
-        $('#productInputDESCRIPTION').focus();
-        return false;
-    } else {
-      $.post("./functions.php", {
-        sub:"add_product_form",
-        sub_index: index,
-        sub_name: name,
-        sub_description: description,
-        sub_unitprice: unitprice,
-        sub_remarks: remarks,
-        sub_clients: clients
-      });
-    }
+  if (name.trim() == '') {
+    alert('Please enter product name.');
+    $('#productInputINDEX').focus();
+    return false;
+  } else if (description.trim() == '') {
+    alert('Please enter description address.');
+    $('#productInputDESCRIPTION').focus();
+    return false;
+  } else {
+    $.post("./functions.php", {
+      sub: "add_product_form",
+      sub_index: index,
+      sub_name: name,
+      sub_description: description,
+      sub_unitprice: unitprice,
+      sub_remarks: remarks,
+      sub_clients: clients
+    });
+  }
 }
 
-function submitEditProductForm(){
+function submitEditProductForm() {
   var id = $('#productID').val();
   var name = $('#productInputNAME').val();
   var description = $('#productInputDESCRIPTION').val();
   var unitprice = $('#productInputUNITPRICE').val();
   var remarks = $('#productInputREMARKS').val();
 
-  if(name.trim() == '' ){
-        alert('Please enter product name.');
-        $('#edit_InputNAME').focus();
-        return false;
-    }else if(description.trim() == '' ){
-        alert('Please enter product description.');
-        $('#productInputDESCRIPTION').focus();
-        return false;
-    } else {
-      
-      $.post("./functions.php", {
-        sub:"edit_product_form",
-        sub_id: id,
-        sub_name: name,
-        sub_description: description,
-        sub_unitprice: unitprice,
-        sub_remarks: remarks
-      });      
-    }
+  if (name.trim() == '') {
+    alert('Please enter product name.');
+    $('#edit_InputNAME').focus();
+    return false;
+  } else if (description.trim() == '') {
+    alert('Please enter product description.');
+    $('#productInputDESCRIPTION').focus();
+    return false;
+  } else {
+
+    $.post("./functions.php", {
+      sub: "edit_product_form",
+      sub_id: id,
+      sub_name: name,
+      sub_description: description,
+      sub_unitprice: unitprice,
+      sub_remarks: remarks
+    });
+  }
 }
 
-function deleteProduct(id){
+function deleteProduct(id) {
   $.post("./functions.php", {
-    sub:"delete_product",
+    sub: "delete_product",
     sub_id: id
-  });   
+  });
 }
 
-function filter_table(userInput, filtering_table, num_of_columns ) {
+function filter_table(userInput, filtering_table, num_of_columns) {
   // Declare variables
   var input, filter, table, tr, td, i, txtValue;
   input = document.getElementById(userInput);
@@ -158,10 +158,10 @@ function filter_table(userInput, filtering_table, num_of_columns ) {
 
   // Loop through all table rows, and hide those who don't match the search query
   for (i = 1; i < tr.length; i++) {
-      tr[i].style.display = "none";
-    }
+    tr[i].style.display = "none";
+  }
 
-  for (j=0; j<num_of_columns; j++) {
+  for (j = 0; j < num_of_columns; j++) {
     for (i = 0; i < tr.length; i++) {
       td = tr[i].getElementsByTagName("td")[j];
       if (td) {
@@ -174,33 +174,60 @@ function filter_table(userInput, filtering_table, num_of_columns ) {
   }
 }
 
+function add_row() {
+
+  var t = $('#invoices_table').DataTable();
+  var counter = t.rows().count() + 1;
+  // Client Name Field
+  client_name_field = "<select id = \"invoiceSelectCLIENT_" + String(counter) + "\" class=\"selectpicker\" data-width=\"100%\" data-live-search=\"true\" data-actions-box = \"true\" data-none-selected-text=\"Select Client\" onchange=\"select_product(" + counter + ")\"></select>";
+  quantity_field = "<input id = \"inviceQUANTITY_" + String(counter) + "\" type=\"text\" class=\"typeahead form-control table-cell text-white\" autocomplete=\"off\" spellcheck=\"false\" value=\"25\" onchange=\"calculate_amount()\">";
+  product_field = "<select id = \"invoiceSelectPRODUCT_" + String(counter) + "\" class=\"selectpicker\" data-width=\"100%\" data-live-search=\"true\" data-actions-box = \"true\" data-none-selected-text=\"Select Product\" onchange=\"calculate_amount(" + counter + ")\"></select> ";
+  amount_field = "<input readonly id = \"invoiceAMOUNT_" + String(counter) + "\" class=\"table-cell text-white\" value=0 style=\"text-align: right\">";
+  total_outstanding_field = "<input readonly id = \"invoiceOUTSTANDING_" + String(counter) + "\" class=\"table-cell text-white\"  value=1467258.0 style=\"text-align: right\">";
+
+  t.row.add([
+    counter,
+    client_name_field,
+    quantity_field,
+    product_field,
+    amount_field,
+    total_outstanding_field
+  ]).draw(false);
+
+  populate_select_field("invoiceSelectCLIENT_" + String(counter));
+  populate_select_field("invoiceSelectPRODUCT_" + String(counter));
+  // counter++;
+  (".selectpicker").selectpicker().selectpicker("render");
+
+}
+
 // Populate a table using ajax
 function Populate_table(str) {
   var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (this.readyState == 4 && this.status == 200) {
-                document.getElementById(str).innerHTML = this.responseText;
-            }
-        };
-        xmlhttp.open("GET", "functions.php?q="+str, true);
-        xmlhttp.send(); 
-        refresh_datatable(str);
+  xmlhttp.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById(str).innerHTML = this.responseText;
+    }
+  };
+  xmlhttp.open("GET", "functions.php?q=" + str, true);
+  xmlhttp.send();
+  refresh_datatable(str);
 }
 
 function refresh_datatable(str) {
-  try{
-    $('#'+str).DataTable().destroy();
-      setTimeout(() => { 
-        $('#'+str).DataTable({
-          "searching" : false
-        });
-      }, 100);
-    } catch(Exception){
-    }
+  try {
+    $('#' + str).DataTable().destroy();
+    setTimeout(() => {
+      $('#' + str).DataTable({
+        "searching": false
+      });
+    }, 100);
+  } catch (Exception) {
+  }
 }
 
 
-$(document).ready(function(){
+$(document).ready(function () {
 
   // --------------- Clients ---------------//
 
@@ -215,25 +242,25 @@ $(document).ready(function(){
     //var modal = $(this);
     //modal.find('#InputID').val(client_id);
 
-      // AJAX request
+    // AJAX request
     $.ajax({
       url: './edit_client.php',
       type: 'POST',
-      data: {clientid: client_id},
+      data: { clientid: client_id },
       //contentType: "text/plain",
-      success: function(response) { //we got the response
-          //alert('Successfully called');
-          $('.editform-modal-body').html(response);
+      success: function (response) { //we got the response
+        //alert('Successfully called');
+        $('.editform-modal-body').html(response);
       },
-      error: function(jqxhr, status, exception) {
-          alert('Exception:', exception);
+      error: function (jqxhr, status, exception) {
+        alert('Exception:', exception);
       }
     });
     //});
   });
 
   $('#editClientModal').on('hidden.bs.modal', function (event) {
-    Populate_table("active_clients_table");    
+    Populate_table("active_clients_table");
   });
 
   $('#deleteClientModal').on('show.bs.modal', function (event) {
@@ -245,37 +272,37 @@ $(document).ready(function(){
     //var modal = $(this);
     //modal.find('#InputID').val(client_id);
     var footer = "<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">No</button>";
-    footer += "<button type=\"submit\" class=\"btn btn-primary submitBtn\" data-toggle=\"modal\" data-target=\"#deleteClientModal\" onclick=\"deleteClient("+client_id+")\">Yes</button>";
-  
+    footer += "<button type=\"submit\" class=\"btn btn-primary submitBtn\" data-toggle=\"modal\" data-target=\"#deleteClientModal\" onclick=\"deleteClient(" + client_id + ")\">Yes</button>";
 
-      // AJAX request
+
+    // AJAX request
     $.ajax({
       url: './confirm_delete.php',
       type: 'POST',
-      data: {type: "client", id: client_id},
+      data: { type: "client", id: client_id },
       //contentType: "text/plain",
-      success: function(response) { //we got the response
-          //alert('Successfully called');
-          $('.delete-client-modal-body').html(response);
+      success: function (response) { //we got the response
+        //alert('Successfully called');
+        $('.delete-client-modal-body').html(response);
 
-          $('.delete-client-modal-footer').html(footer);
+        $('.delete-client-modal-footer').html(footer);
       },
-      error: function(jqxhr, status, exception) {
-          alert('Exception:', exception);
+      error: function (jqxhr, status, exception) {
+        alert('Exception:', exception);
       }
     });
     //});
   });
 
   $('#deleteClientModal').on('hidden.bs.modal', function (event) {
-    Populate_table("active_clients_table");    
+    Populate_table("active_clients_table");
   });
 
   $('#addClientModal').on('hidden.bs.modal', function (event) {
     Populate_table("active_clients_table");
   });
 
-    // --------------- Products ---------------//
+  // --------------- Products ---------------//
 
   $('#editProductModal').on('show.bs.modal', function (event) {
 
@@ -288,18 +315,18 @@ $(document).ready(function(){
     //var modal = $(this);
     //modal.find('#InputID').val(client_id);
 
-      // AJAX request
+    // AJAX request
     $.ajax({
       url: './edit_product.php',
       type: 'POST',
-      data: {productid: product_id},
+      data: { productid: product_id },
       //contentType: "text/plain",
-      success: function(response) { //we got the response
-          //alert('Successfully called');
-          $('.editproductform-modal-body').html(response);
+      success: function (response) { //we got the response
+        //alert('Successfully called');
+        $('.editproductform-modal-body').html(response);
       },
-      error: function(jqxhr, status, exception) {
-          alert('Exception:', exception);
+      error: function (jqxhr, status, exception) {
+        alert('Exception:', exception);
       }
     });
     //});
@@ -307,7 +334,7 @@ $(document).ready(function(){
 
   $('#editProductModal').on('hidden.bs.modal', function (event) {
     Populate_table("active_clients_table");
-    Populate_table("products_table"); 
+    Populate_table("products_table");
   });
 
   $('#deleteProductModal').on('show.bs.modal', function (event) {
@@ -319,30 +346,30 @@ $(document).ready(function(){
     //var modal = $(this);
     //modal.find('#InputID').val(client_id);
     var footer = "<button type=\"button\" class=\"btn btn-secondary\" data-dismiss=\"modal\">No</button>";
-    footer += "<button type=\"submit\" class=\"btn btn-primary submitBtn\" data-toggle=\"modal\" data-target=\"#deleteProductModal\" onclick=\"deleteProduct("+product_id+")\">Yes</button>";
-  
+    footer += "<button type=\"submit\" class=\"btn btn-primary submitBtn\" data-toggle=\"modal\" data-target=\"#deleteProductModal\" onclick=\"deleteProduct(" + product_id + ")\">Yes</button>";
 
-      // AJAX request
+
+    // AJAX request
     $.ajax({
       url: './confirm_delete.php',
       type: 'POST',
-      data: {type: "product", id: product_id},
+      data: { type: "product", id: product_id },
       //contentType: "text/plain",
-      success: function(response) { //we got the response
-          //alert('Successfully called');
-          $('.delete-product-modal-body').html(response);
+      success: function (response) { //we got the response
+        //alert('Successfully called');
+        $('.delete-product-modal-body').html(response);
 
-          $('.delete-product-modal-footer').html(footer);
+        $('.delete-product-modal-footer').html(footer);
       },
-      error: function(jqxhr, status, exception) {
-          alert('Exception:', exception);
+      error: function (jqxhr, status, exception) {
+        alert('Exception:', exception);
       }
     });
     //});
   });
 
   $('#deleteProductModal').on('hidden.bs.modal', function (event) {
-    Populate_table("products_table");    
+    Populate_table("products_table");
   });
 
   $('#addProductModal').on('hidden.bs.modal', function (event) {
@@ -352,15 +379,22 @@ $(document).ready(function(){
   // --------------- Activating DataTables for Regular Bootstrap tables ---------------//
 
   $('#products_table').DataTable({
-    "searching" : false
+    "searching": false
   });
 
   $('#clients_table').DataTable({
-    "searching" : false
+    "searching": false
   });
 
   $('#active_clients_table').DataTable({
-    "searching" : false
-  });  
-  
+    "searching": false
+  });
+
+  // Invoices Table
+  $('#invoices_table').DataTable({
+    "searching": false
+  });
+
+  add_row();
+
 });
