f66ab90472a3c2199b0492575d8383f0bbb3be35
RomeshEtulgama <etulgamaromesh79@gmail.com>
2020-05-01 06:03:59 +0530

    dashboard, reports not finished. db connection not finished.

--- web_interface/indexy.php
+++ web_interface/indexy.php
@@ -14,13 +14,13 @@
   <title>Quentin Interface</title>
 
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-  <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
+  <script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
   <link rel="stylesheet" href="css/style.css">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="css/font-awesome.min.css">
 
   <script src="./index_files/ie-emulation-modes-warning.js.download"></script>
 
@@ -29,12 +29,12 @@
   <?php include('functions.php'); ?>
 
   <!-- BOOTSTRAP SELECT PLUGIN -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/css/bootstrap-select.min.css">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap-select@1.13.9/dist/js/bootstrap-select.min.js"></script>
+  <link rel="stylesheet" href="css/bootstrap-select.min.css">
+  <script src="js/bootstrap-select.min.js"></script>
 
   <!-- Datepicker -->
-  <script src="https://unpkg.com/gijgo@1.9.13/js/gijgo.min.js" type="text/javascript"></script>
-  <link href="https://unpkg.com/gijgo@1.9.13/css/gijgo.min.css" rel="stylesheet" type="text/css" />
+  <script src="js/gijgo.min.js" type="text/javascript"></script>
+  <link href="css/gijgo.min.css" rel="stylesheet" type="text/css" />
 
   <!-- Custom styles for this template -->
   <link href="css/simple-sidebar.css" rel="stylesheet">
@@ -80,15 +80,13 @@
           <?php include("public/products.php"); ?>
         </div>
 
-        <div class="container" id="Invoices_area" style="max-width: 200%">
+        <div class="container" id="Invoices_area" style="max-width: 100%">
           <?php include("public/invoices.php"); ?>
         </div>
 
         <div class="container" id="Payments_area" style="max-width: 100%">
           <?php include("public/payments.php"); ?>
         </div>
-
-
       </div>
     </div>
 
@@ -103,15 +101,15 @@
     });
     $("#Clients_area").hide();
     $("#Products_area").hide();
-    $("#Invoices_area").hide();
-    // $("#Payments_area").hide();
+    // $("#Invoices_area").hide();
+    $("#Payments_area").hide();
   </script>
 
   <!-- Placed at the end of the document so the pages load faster -->
   <script src="./index_files/bootstrap.min.js.download"></script>
 
   <!-- DataTables -->
-  <script type="text/javascript" src="./js/dataTables.js"></script>
+  <script type="text/javascript" src="./js/dataTables.min.js"></script>
   <!-- <script type="text/javascript" src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script> -->
 
   <!-- Typeahead -->
