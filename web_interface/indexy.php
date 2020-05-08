0f2cfa9257566d41d022fb7dfed2164268198415
RomeshEtulgama <etulgamaromesh79@gmail.com>
2020-05-08 07:53:41 +0530

    header bug fixed

--- web_interface/indexy.php
+++ web_interface/indexy.php
@@ -14,10 +14,10 @@
   <title>Quentin Interface</title>
 
   <!-- Bootstrap core CSS -->
-  <link rel="stylesheet" href="css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/bootstrap.min.css" >
   <script src="js/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
-  <script src="js/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  <script src="js/popper.min.js" ></script>
+  <script src="js/bootstrap.min.js" ></script>
 
   <link rel="stylesheet" href="css/style.css">
   <link rel="stylesheet" href="css/font-awesome.min.css">
